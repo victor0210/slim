4a3fd312d0e6ac16b61b84fef3631f3ee9d2f21c
humengtao <humengtao@bytedance.com>
2019-01-30 11:21:33 +0800

    Release 2.0.2

--- build/release.sh
+++ build/release.sh
@@ -6,7 +6,6 @@ echo $package
 
 mkdir -p release/$package
 
-cp -R src/$package/* ./release/$package
 cp LICENSE README.md .release-it.json ./release/$package/
 mkdir -p release/$package/dist
 cp -R ./dist/release/$package/* ./release/$package/dist
