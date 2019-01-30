2dd0cafa082c26bed863ab2bf4f74113d7f6a0ca
humengtao <humengtao@bytedance.com>
2019-01-30 11:24:02 +0800

    update npm package

--- build/release.sh
+++ build/release.sh
@@ -6,6 +6,7 @@ echo $package
 
 mkdir -p release/$package
 
+cp -R src/$package/package.json ./release/$package
 cp LICENSE README.md .release-it.json ./release/$package/
 mkdir -p release/$package/dist
 cp -R ./dist/release/$package/* ./release/$package/dist
