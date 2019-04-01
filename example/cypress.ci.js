84dc4f5a07bc48d79b7929c89287c8b79191310f
humengtao <humengtao@bytedance.com>
2019-04-01 14:01:39 +0800

    fix: cypress ci config

--- example/cypress.ci.js
+++ example/cypress.ci.js
@@ -2,8 +2,12 @@ const server = require("./server");
 const cypress = require("cypress");
 
 // start your server
-cypress.run().then(() => {
-  // stop your server when it's complete
-  console.log(`cypress test completed!`);
-  process.exit(0);
-});
+cypress
+  .run({
+    browser: "chrome"
+  })
+  .then(() => {
+    // stop your server when it's complete
+    console.log(`cypress test completed!`);
+    process.exit(0);
+  });
